f7300c36605d41903c6bcc0b671427103e20eda5
Real-4W <real_4w@protonmail.com>
2023-08-27 15:38:03 +1200

    WIP

--- w_som_class.py
+++ w_som_class.py
@@ -1,17 +1,23 @@
 class WineRecommendation:
     def __init__(self):
-        # A basic mapping of food to wine variety. This can be expanded.
+        # An expanded mapping of food to wine variety. 
         self.wine_recommendations = {
-            'beef': 'Cabernet Sauvignon',
-            'chicken': 'Chardonnay',
-            'fish': 'Pinot Grigio',
-            'lamb': 'Merlot',
-            'pork': 'Shiraz',
-            'vegetarian': 'Sauvignon Blanc'
+            'beef': ['Cabernet Sauvignon', 'Merlot', 'Shiraz'],
+            'chicken': ['Chardonnay', 'Pinot Noir', 'Sauvignon Blanc'],
+            'grilled fish': ['Pinot Grigio', 'Chardonnay'],
+            'spicy fish': ['Riesling', 'Gewürztraminer'],
+            'lamb': ['Merlot', 'Zinfandel', 'Cabernet Franc'],
+            'pork': ['Shiraz', 'Tempranillo', 'Pinot Noir'],
+            'spicy food': ['Syrah', 'Grenache', 'Viognier'],
+            'vegetarian': ['Sauvignon Blanc', 'Pinot Grigio', 'Chenin Blanc'],
+            'cheese': ['Chardonnay', 'Riesling', 'Cabernet Sauvignon'],
+            'chocolate dessert': ['Port', 'Sherry', 'Merlot'],
+            'fruity dessert': ['Moscato', 'Sauternes', 'Riesling']
         }
 
     def recommend_based_on_food(self, food):
-        return self.wine_recommendations.get(food.lower(), 'Pinot Noir')  # Default to Pinot Noir if food is not in our list
+        # Return a list of recommendations or default to ['Pinot Noir', 'Merlot', 'Cabernet Sauvignon']
+        return self.wine_recommendations.get(food.lower(), ['Pinot Noir', 'Merlot', 'Cabernet Sauvignon'])
 
 
 class VirtualSommelier:
@@ -20,10 +26,12 @@ class VirtualSommelier:
 
     def interact_with_user(self):
         print("Welcome to the Virtual Sommelier!")
-        food = input("What food are you eating? (e.g., beef, chicken, fish): ")
+        food = input("What food are you eating? (e.g., beef, chicken, spicy fish, chocolate dessert): ")
 
-        recommended_wine = self.wine_recommender.recommend_based_on_food(food)
-        print(f"For {food}, I recommend {recommended_wine}.")
+        recommended_wines = self.wine_recommender.recommend_based_on_food(food)
+        print(f"For {food}, I recommend:")
+        for wine in recommended_wines:
+            print(f"- {wine}")
 
 
 if __name__ == "__main__":
